fb17563ddfdc17b59a7efddb409845beba563211
Demonkratiy <demonkratiy@gmail.com>
2020-01-17 18:28:16 +0300

    SwapiService created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import './index.css';
+//import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// fetch('https://swapi.co/api/people/1/')
+//   .then((res) => {//res - response, Fetch работает через промиз, и когда он получит ответ res то выполнит функцию
+//     console.log('Got response', res.status)//на данном этапе мы получаем какойто ответ но еще не тело ответа от сервера
+//     return res.json();
+//   })
+//   .then((body) => {//каждый метод res работает через еще 1 promise, и здесь получив ответ через предыдущий метод, мы обрабатываем его, чтобы получить тело ответа
+//     console.log('Got response', body)
+//   })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*const getResource = async(url) => {
+  const res = await fetch(url); //fetch возвращает promise, await ждет пока результат этого промиса не станет доступным, после чего запишет этот результат в переменную res
+
+  if (!res.ok) {
+    throw new Error(`getResource(${url}) function recived an error with status ${res.status}`);
+  }
+
+  const body = await res.json();
+  return body
+};
+
+getResource('https://swapi.co/api/people/1/')
+  .then((body) => {
+    console.log('Got response', body)
+  })
+  .catch((err) => {
+    console.error(`getResource() function got an error: `, err);
+  });*/
+
+
+class SwapiService {
+  _apiBase = 'https://swapi.co/api'
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`getResource(${url}) function recived an error with status ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllPeople() {
+    return this.getResource(`/people/`).then((res) => {
+      return res.results;//здесь results это название метода в API самой SWAPI
+    });
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+
+  getAllPlanets() {
+    return this.getResource(`/planets/`).then((res) => {
+      return res.results;//здесь results это название метода в API самой SWAPI
+    });
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`)
+  }
+
+  getAllStarships() {
+    return this.getResource(`/starships/`).then((res) => {
+      return res.results;//здесь results это название метода в API самой SWAPI
+    });
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`)
+  }
+
+}
+
+const swapi = new SwapiService();
+
+swapi.getPerson(4).then((p) => {
+  console.log(p.name);
+})
+
+swapi.getAllStarships().then((p) => {
+  p.forEach((p) => {
+  console.log(p.name);
+  })
+})
+
+//ReactDOM.render(<App />, document.getElementById('root'));
